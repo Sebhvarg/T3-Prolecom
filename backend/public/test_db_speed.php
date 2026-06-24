56346647d076676ca2b417d09e565cdadf6245c9
Sebhvarg <sebhvarg@espol.edu.ec>
2026-06-24 09:11:58 -0500

    Fix: Sonner issues

--- backend/public/test_db_speed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$t = microtime(true);
-try {
-    $pdo = new PDO('mysql:host=db;port=3306;dbname=prolecom', 'root', 'root');
-    echo 'Connected in '.(microtime(true) - $t).' seconds';
-} catch (Exception $e) {
-    echo 'Error: '.$e->getMessage().' in '.(microtime(true) - $t).' seconds';
-}
