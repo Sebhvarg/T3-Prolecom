9c6b0634272ca3d6aa8a4ce0dd2f54e861fc9f50
Sebhvarg <sebhvarg@espol.edu.ec>
2026-06-26 10:40:25 -0500

    style: fix pint code style and merge master

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,7 +20,6 @@ if (! defined('ROUTE_DESAFIO_ID')) {
 
 Route::post('/login', [AuthController::class, 'login']);
 
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
